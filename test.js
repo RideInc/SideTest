4b3c344871de3554eaabc406f87a7ac7e4fb18ac
SideFx671 <serj.ride@yandex.ru>
2020-12-22 12:42:47 +0300

    add CVE module

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-//Done
+import CVE
+
 const frame = {
    recipient : 88.123.44.4,
    sender    : 410.212.23.23,
@@ -10,13 +11,25 @@ const frame = {
 const bruteForce = (frame) => {
   let count = frame.data.length;
   let i = 0;
+  let output = [];
   while (i < count) {
 
-    console.log(frame.data[i]);
+    return frame.data[i]
 
     i++;
   };
-
 };
 
-bruteForce(frame);
+const checkForce = (data) => {
+  let i = 0;
+  while (i < data.length) {
+
+    if (data[i] in CVE) continue;
+    else
+
+    i++
+  };
+};
+
+let force = bruteForce(frame); // извлекаем данные
+checkForce(force); // проверяем данные
